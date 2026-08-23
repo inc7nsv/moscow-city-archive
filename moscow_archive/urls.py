cf30e5b3fbad26a43e36166a2ee01a24100f4655
i-chernousov <iche7n@gmail.com>
2026-08-23 18:11:09 +0300

    Update urls.py

--- moscow_archive/urls.py
+++ moscow_archive/urls.py
@@ -2,21 +2,28 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import include, path, re_path
-
-from archive_app import views as archive_views
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('archive_app.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name='registration/login.html'
+        ),
+        name='login'
+    ),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(),
+        name='logout'
+    ),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# Catch-all расположен последним: любая неизвестная страница получает оформленный ответ 404.
-urlpatterns += [re_path(r'^.*$', archive_views.custom_404)]
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
 
 handler404 = 'archive_app.views.custom_404'
